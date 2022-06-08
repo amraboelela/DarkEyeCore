1f18b946ea3d00754b82c6123014e9dc74f61a76
Amr Aboelela <amraboelela@gmail.com>
2022-06-08 15:24:34 -0700

    Update Link.swift

--- Sources/DarkEyeCore/Models/Link.swift
+++ Sources/DarkEyeCore/Models/Link.swift
@@ -9,19 +9,15 @@
 import Foundation
 import SwiftLevelDB
 
-/*public struct LinksContainer: Codable {
-    public var links: [Link]
-}*/
-
 public struct Link: Codable {
     public static let prefix = "link-"
-    public static var links = [Link]()
+    public static var processTimeThreshold = 1 // any link with last process time smaller, need to be processed
     
     public var url: String
     public var title: String?
-    public var lastProcessTime: Int? // # of seconds since reference date.
+    public var lastProcessTime: Int = 0 // # of seconds since reference date.
     public var numberOfVisits: Int = 0
-    public var lastVisitTime: Int? // # of seconds since reference date.
+    public var lastVisitTime: Int = 0 // # of seconds since reference date.
     
     // MARK: - Accessors
     
@@ -48,6 +44,70 @@ public struct Link: Codable {
         return Link(url: url(fromKey: key))
     }
 
+    
+    public static func links(
+        withSearchText searchText: String,
+        time: Int? = nil,
+        before: Bool = true,
+        count: Int
+    ) -> [Link] {
+        var result = [Link]()
+        let searchWords = Word.words(fromText: searchText)
+        if let firstWord = searchWords.first {
+            var wordPostKeys = [String]()
+            database.enumerateKeysAndValues(backward: true, startingAtKey: nil, andPrefix: Word.prefix + firstWord) { (key, word: Word, stop) in
+                if time == nil {
+                    wordPostKeys.append(word.postKey)
+                } else if let time = time {
+                    if before {
+                        if Word.time(fromKey: key) <= time {
+                            wordPostKeys.append(word.postKey)
+                        }
+                    } else {
+                        if Word.time(fromKey: key) >= time {
+                            wordPostKeys.append(word.postKey)
+                        }
+                    }
+                }
+            }
+            for wordPostKey in wordPostKeys {
+                var foundTheSearch = true
+                if let link: Link = database[wordPostKey] {
+                    for i in 1..<searchWords.count {
+                        let searchWord = searchWords[i]
+                        if link.title?.lowercased().range(of: searchWord) == nil {
+                            foundTheSearch = false
+                            break
+                        }
+                    }
+                    if foundTheSearch {
+                        result.append(link)
+                    }
+                }
+            }
+            result = result.sorted { $0.numberOfVisits > $1.numberOfVisits }
+            if result.count > count {
+                result.removeLast(result.count - count)
+            }
+        } /*else {
+            //logger.log("getPosts, searchText is empty")
+            var startAtKey: String? = nil
+            if let time = time {
+                startAtKey = prefix + "\(time)"
+            } else {
+                database.enumerateKeysAndValues(backward: before, startingAtKey: startAtKey, andPrefix: prefix) { (key, post: Post, stop) in
+                    if result.count < count {
+                        result.append(post)
+                    } else {
+                        stop.pointee = true
+                    }
+                }
+            }
+            
+        }*/
+        return result
+    }
+    
     // MARK: - Saving data
     
     public static func save(links: [Link]) -> Int {
