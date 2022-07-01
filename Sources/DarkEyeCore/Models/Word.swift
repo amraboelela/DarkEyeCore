212c4890c24f9ca63e5e02a2795eda04e8d75b73
Amr Aboelela <amraboelela@gmail.com>
2022-07-01 16:50:11 -0700

    print("wordText: \(wordText)")

--- Sources/DarkEyeCore/Models/Word.swift
+++ Sources/DarkEyeCore/Models/Word.swift
@@ -25,7 +25,7 @@ public struct Word: Codable {
             }
             let text = contextStringFrom(array: wordsArray, atIndex: i)
             let wordText = wordsArray[i]
-            //print("wordText: \(wordText)")
+            print("wordText: \(wordText)")
             if wordText.count > 2 {
                 let key = prefix + wordText.lowercased()
                 //print("index link key: \(key)")
