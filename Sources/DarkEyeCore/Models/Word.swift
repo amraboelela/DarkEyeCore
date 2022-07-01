21c9715c8d164c401617c884dec4859cc6c58627
Amr Aboelela <amraboelela@gmail.com>
2022-07-01 16:46:12 -0700

    //print("index link key: \(key)")

--- Sources/DarkEyeCore/Models/Word.swift
+++ Sources/DarkEyeCore/Models/Word.swift
@@ -25,18 +25,18 @@ public struct Word: Codable {
             }
             let text = contextStringFrom(array: wordsArray, atIndex: i)
             let wordText = wordsArray[i]
-            print("wordText: \(wordText)")
+            //print("wordText: \(wordText)")
             if wordText.count > 2 {
                 let key = prefix + wordText.lowercased()
-                print("index link key: \(key)")
+                //print("index link key: \(key)")
                 let word = Word(links: [WordLink(url: link.url, title: link.title, text: text, wordCount: counts[wordText.lowercased()] ?? 0)])
                 if var dbWord: Word = database[key] {
                     WordLink.merge(wordLinks: &dbWord.links, withWordLinks: word.links)
                     database[key] = dbWord
-                    print("database[key] = dbWord: \(dbWord)")
+                    //print("database[key] = dbWord: \(dbWord)")
                 } else {
                     database[key] = word
-                    print("database[key] = word: \(word)")
+                    //print("database[key] = word: \(word)")
                 }
             }
         }
