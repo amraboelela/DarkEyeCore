3478b458ae9f9680f50082933c7ab6c257bfc843
Amr Aboelela <amraboelela@gmail.com>
2022-07-03 20:32:38 -0700

    Update Crawler.swift

--- Sources/DarkEyeCore/Crawler.swift
+++ Sources/DarkEyeCore/Crawler.swift
@@ -34,13 +34,13 @@ public class Crawler {
         }
         serialQueue.async {
             Link.crawlNext()
-            if self.canRun && Link.numberOfProcessedLinks < 2 {
+            /*if self.canRun && Link.numberOfProcessedLinks < 1 {
                 self.serialQueue.async {
                     self.crawl()
                 }
             } else {
                 self.delegate?.crawlerStopped()
-            }
+            }*/
         }
     }
     
