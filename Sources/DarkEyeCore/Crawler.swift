c314e0c310db342ee08e035e1399c2867bf9e34f
Amr Aboelela <amraboelela@gmail.com>
2022-07-11 14:56:16 -0700

    Update Crawler.swift

--- Sources/DarkEyeCore/Crawler.swift
+++ Sources/DarkEyeCore/Crawler.swift
@@ -28,6 +28,16 @@ public class Crawler {
             NSLog("Crawler init, creating: " + Link.prefix + Link.mainUrl)
             var link = Link(url: Link.mainUrl)
             link.save()
+            database.enumerateKeysAndValues(backward: false, startingAtKey: nil, andPrefix: Link.prefix) { (Key, link: Link, stop) in
+                NSLog("nextLinkToProcess, Key: \(Key)")
+                //if link.lastProcessTime < processTimeThreshold &&
+                //  link.linkAddedTime < processTimeThreshold {
+                //  stop.pointee = true
+                //result = link
+                /*} else {
+                 NSLog("nextLinkToProcess else, Key: \(Key)")
+                 }*/
+            }
         }
     }
     
