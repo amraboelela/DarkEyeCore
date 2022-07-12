7228cb88321a434ce96f98f180a1798fa7926a3d
Amr Aboelela <amraboelela@gmail.com>
2022-07-12 08:10:01 -0700

    Update Word.swift

--- Sources/DarkEyeCore/Models/Word.swift
+++ Sources/DarkEyeCore/Models/Word.swift
@@ -20,10 +20,10 @@ public struct Word: Codable {
         //return true
         var processedKeys = Set<String>()
         var wordsArray = words(fromText: link.text)
-        let countLimit = 100
+        /*let countLimit = 100
         if wordsArray.count > countLimit {
             wordsArray.removeLast(wordsArray.count - countLimit)
-        }
+        }*/
         let counts = wordsArray.reduce(into: [:]) { counts, word in counts[word.lowercased(), default: 0] += 1 }
         NSLog("indexing wordsArray.count: \(wordsArray.count)")
         for i in (0..<wordsArray.count) {
