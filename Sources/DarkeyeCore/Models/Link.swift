e49d6cee79c6efc844c7d5312090bbe79c2897bd
Amr Aboelela <amraboelela@gmail.com>
2022-09-13 15:24:13 -0700

    Update Link.swift

--- Sources/DarkeyeCore/Models/Link.swift
+++ Sources/DarkeyeCore/Models/Link.swift
@@ -300,7 +300,7 @@ public struct Link: Codable, Equatable, Sendable {
             if var link: Link = await database.value(forKey: Link.prefix + childURL) {
                 link.numberOfLinks += 1
                 NSLog("link.url: \(link.url)")
-                NSLog("link.numberOfLinks: \(link.numberOfLinks)")
+                //NSLog("link.numberOfLinks: \(link.numberOfLinks)")
                 await link.save()
             } else {
                 var link = Link(url: childURL)
