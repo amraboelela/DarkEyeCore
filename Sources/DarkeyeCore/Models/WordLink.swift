afc868cd25b594404e5c2c54d3f109ecbd446c41
Amr Aboelela <amraboelela@gmail.com>
2022-09-18 10:33:25 -0700

    wordLinks withSearchText

--- Sources/DarkeyeCore/Models/WordLink.swift
+++ Sources/DarkeyeCore/Models/WordLink.swift
@@ -108,14 +108,16 @@ public struct WordLink: Codable, Hashable, Sendable {
         withSearchText searchText: String,
         count: Int
     ) async -> [WordLink] {
-        
+        NSLog("wordLinks with searchText: \(searchText)")
         let searchTextEncoded = searchText.lowercased().replacingOccurrences(of: " ", with: "+") // e.g. war+on+ukrain
         let torchURL = "http://torchdeedp3i2jigzjdmfpn5ttjhthh5wbmda2rr3jvqjg5p77c54dqd.onion/search?query=" + searchTextEncoded // e.g. http://torchdeedp3i2jigzjdmfpn5ttjhthh5wbmda2rr3jvqjg5p77c54dqd.onion/search?query=war+on+ukrain
         var torchLink = Link(url: torchURL, isSearchEngline: true, priority: .high)
+        NSLog("wordLinks withSearchText, torchLink: \(torchLink)")
         await torchLink.save()
         
         let ahmiaURL = "http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/search/?q=" + searchTextEncoded // e.g. http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/search/?q=war+on+ukrain
         var ahmiaLink = Link(url: ahmiaURL, isSearchEngline: true, priority: .high)
+        NSLog("wordLinks withSearchText, ahmiaLink: \(ahmiaLink)")
         await ahmiaLink.save()
         
         var resultSet = Set<WordLink>()
